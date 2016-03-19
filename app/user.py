83b7fb57ddf3a43b971d1a21df404bf835203cf9
Ксения Сначева <Snach@MacBook-Air-Ksenia.local>
2016-03-19 21:57:23 +0300

    list_followers/list_following in user done, YEAH!

--- app/user.py
+++ app/user.py
@@ -53,10 +53,6 @@ def details_user():
         return jsonify({'code': 1, 'response': 'User not found '})
     return jsonify({'code': 0, 'response': user})
 
-
-
-
-
 @app.route('/db/api/user/follow', methods=['POST'])
 def create_follow():
     try:
@@ -78,8 +74,152 @@ def create_follow():
         )
     except MySQLdb.Error:
         return jsonify({'code': 3, 'response': "Incorrect request"})
+    db.commit()
     user = functions.user_details(cursor, content_json['follower'])
     return jsonify({'code': 0, 'response': user})
+
+@app.route('/db/api/user/updateProfile', methods=['POST'])
+def update_user():
+    try:
+        content_json = request.json
+    #    print content_json
+    except BadRequest:
+        return jsonify({'code': 2, 'response': "Invalid request(syntax)"})
+    if 'user' not in content_json or 'about' not in content_json or 'name' not in content_json:
+        return jsonify({'code': 3, 'response':  "Incorrect request: some data missing"})
+
+    db = mysql.get_db()
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    try:
+        cursor.execute(
+            """UPDATE `users` SET `about` = %s, `name` = %s WHERE `email` = %s;""",
+            (
+                content_json['about'],
+                content_json['name'],
+                content_json['user'])
+        )
+    except MySQLdb.Error:
+        return jsonify({'code': 3, 'response': "Incorrect request: user is already exist"})
+    db.commit()
+    user = functions.user_details(cursor, content_json['user'])
+    return jsonify({'code': 0, 'response': user})
+
+@app.route('/db/api/user/unfollow', methods=['POST'])
+def delete_follow():
+    try:
+        content_json = request.json
+    #    print content_json
+    except BadRequest:
+        return jsonify({'code': 2, 'response': "Invalid request(syntax)"})
+    if 'follower' not in content_json or 'followee' not in content_json:
+        return jsonify({'code': 3, 'response':  "Incorrect request: some data missing"})
+
+    db = mysql.get_db()
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    try:
+        cursor.execute(
+            """DELETE FROM `followers`
+                WHERE `follower` = %s and `followee` = %s;""",
+            (
+                content_json['follower'],
+                content_json['followee'])
+        )
+    except MySQLdb.Error:
+        return jsonify({'code': 3, 'response': "Incorrect request"})
+    db.commit()
+    user = functions.user_details(cursor, content_json['follower'])
+    return jsonify({'code': 0, 'response': user})
+
+@app.route('/db/api/user/listFollowers/', methods=['GET'])
+def list_followers():
+    user_email = request.args.get('user', None)
+    order = request.args.get('order', 'DESC')
+    limit = request.args.get('limit', None)
+    since_id = request.args.get('since_id', '1')
+
+    if user_email is None:
+        return jsonify({'code': 1, 'response': "User not found "})
+
+    if limit is None:
+        limit = " "
+    else:
+        limit = 'LIMIT' + limit
+
+
+    db = mysql.get_db()
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    try:
+        cursor.execute(
+            """SELECT `about`, `email`, `id`, `isAnonymous`, `name`, `username` FROM `followers` AS `f`
+                JOIN `users` ON `users`.`email` = `f`.`follower`
+                WHERE `f`.`followee` = %s AND `users`.`id` >= %s
+                ORDER BY `name` """ + order + limit + " ;",
+            (
+                user_email,
+                int(since_id)
+            )
+        )
+    except MySQLdb.Error:
+        return jsonify({'code': 3, 'response': "Incorrect request"})
+    users = [i for i in cursor.fetchall()]
+
+    for user in users:
+        following = functions.list_following(cursor, user['email'])
+        followers = functions.list_followers(cursor, user['email'])
+
+        cursor.execute("""SELECT `thread` FROM `subscriptions` WHERE `user` = %s;""", (user['email'],))
+        threads = [i['thread'] for i in cursor.fetchall()]
+
+        user.update({'following': following, 'followers': followers, 'subscriptions': threads})
+
+    return jsonify({'code': 0, 'response': users})
+
+@app.route('/db/api/user/listFollowing/', methods=['GET'])
+def list_following():
+    user_email = request.args.get('user', None)
+    order = request.args.get('order', 'DESC')
+    limit = request.args.get('limit', None)
+    since_id = request.args.get('since_id', '1')
+
+    if user_email is None:
+        return jsonify({'code': 1, 'response': "User not found "})
+
+    if limit is None:
+        limit = " "
+    else:
+        limit = 'LIMIT' + limit
+
+
+    db = mysql.get_db()
+    cursor = db.cursor(MySQLdb.cursors.DictCursor)
+
+    try:
+        cursor.execute(
+            """SELECT `about`, `email`, `id`, `isAnonymous`, `name`, `username` FROM `followers` AS `f`
+                JOIN `users` ON `users`.`email` = `f`.`followee`
+                WHERE `f`.`follower` = %s AND `users`.`id` >= %s
+                ORDER BY `name` """ + order + limit + " ;",
+            (
+                user_email,
+                int(since_id)
+            )
+        )
+    except MySQLdb.Error:
+        return jsonify({'code': 3, 'response': "Incorrect request"})
+    users = [i for i in cursor.fetchall()]
+    for user in users:
+        following = functions.list_following(cursor, user['email'])
+        followers = functions.list_followers(cursor, user['email'])
+
+        cursor.execute("""SELECT `thread` FROM `subscriptions` WHERE `user` = %s;""", (user['email'],))
+        threads = [i['thread'] for i in cursor.fetchall()]
+
+        user.update({'following': following, 'followers': followers, 'subscriptions': threads})
+
+    return jsonify({'code': 0, 'response': users})
 
 
 
