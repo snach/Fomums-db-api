94a2ed56aba76b255462f8076c08caccc889f12a
Ксения Сначева <Snach@MacBook-Air-Ksenia.local>
2016-04-12 22:49:39 +0300

    fix utf8

--- app/__init__.py
+++ app/__init__.py
@@ -8,6 +8,8 @@ app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'rootPassword'
 app.config['MYSQL_DATABASE_DB'] = 'db_Forums'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_CHARSET'] = 'utf-8'
+
 mysql.init_app(app)
 
 from app import views
